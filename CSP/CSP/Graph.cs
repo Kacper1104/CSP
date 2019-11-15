faa53732458bcada59e1541c83758eab0eaf2b00
Kacper1104 <kacper.kozimor@gmail.com>
2019-11-15 14:19:01 +0100

    documents from diagnostics

--- CSP/CSP/Graph.cs
+++ CSP/CSP/Graph.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace CSP
 {
@@ -11,6 +12,13 @@ namespace CSP
         public Node initialNode { get; set; }
         public List<Node> btSolutions;
         public List<Node> fcSolutions;
+
+        //Diagnostics
+        long nodesVisited;
+        long time;
+        Stopwatch stopwatch;
+        long generalTime;
+        Stopwatch generalStopwatch;
 
         public Graph(int problemSize, int problem)
         {
@@ -38,12 +46,29 @@ namespace CSP
 
         #region FIBONACCI_PROBLEM
 
-        public void FibonacciForwardChecking(int heuristicID) 
+        public void FibonacciForwardChecking(int heuristicID, int solutionsLimit) 
         {
-            FibonacciForwardChecking(initialNode, heuristicID);
+            //Diagnostics
+            stopwatch = new Stopwatch();
+            time = 0;
+            nodesVisited = 0;
+            stopwatch.Start();
+            generalStopwatch = new Stopwatch();
+            generalTime = 0;
+            generalStopwatch.Start();
+            //Diagnostics end
+            FibonacciForwardChecking(initialNode, heuristicID, solutionsLimit);
+            Console.WriteLine("Forward Checking finished. Found " + fcSolutions.Count + " solutions");
+            //Diagnostics
+            generalStopwatch.Stop();
+            generalTime = generalStopwatch.ElapsedMilliseconds;
+            Console.WriteLine("Diagnostics for Hetmans Problem: \nTime until first solution: {0}\nNodes visited: {1}\nTime in general: {2}", time, nodesVisited, generalTime);
+            //Diagnostics End
         }
-        public void FibonacciForwardChecking(Node node, int heuristicID)
+        public void FibonacciForwardChecking(Node node, int heuristicID, int solutionsLimit)
         {
+            if (fcSolutions.Count == solutionsLimit && solutionsLimit != -1)
+                return;
             List<int> possibleIndexes = new List<int>();
             bool isNewSolution;
             if (node.SatisfactionCheck() == 0)//if problem not satisfied
@@ -66,11 +91,20 @@ namespace CSP
                 }
                 foreach (var index in possibleIndexes)
                 {
-                    FibonacciForwardChecking(node.InsertToSolution(index), heuristicID);
+                    //Diagnostics
+                    nodesVisited++;
+                    //Diagnostics End
+                    FibonacciForwardChecking(node.InsertToSolution(index), heuristicID, solutionsLimit);
                 }
             }
-            else if (node.SatisfactionCheck() == 1)
+            else if (node.SatisfactionCheck() == 1)//if problem satisfied
             {
+                if(time == 0)//Diagnostics
+                {
+                    stopwatch.Stop();
+                    time = stopwatch.ElapsedMilliseconds;
+                }//Diagnostics End
+
                 isNewSolution = true;
                 foreach (var item in fcSolutions)
                 {
@@ -80,17 +114,33 @@ namespace CSP
                 if (isNewSolution || fcSolutions.Count == 0)
                 {
                     fcSolutions.Add(node);
-                    PrintFibonacciScore(node, "fc");
+                    //PrintFibonacciScore(node, "fc");
                 }
             }
         }
-        public void FibonacciBackTracking(int heuristicID)
+        public void FibonacciBackTracking(int heuristicID, int solutionsLimit)
         {
-            FibonacciBackTracking(initialNode, heuristicID);
+            //Diagnostics
+            stopwatch = new Stopwatch();
+            time = 0;
+            nodesVisited = 0;
+            stopwatch.Start();
+            generalStopwatch = new Stopwatch();
+            generalTime = 0;
+            generalStopwatch.Start();
+            //Diagnostics end
+            FibonacciBackTracking(initialNode, heuristicID, solutionsLimit);
             Console.WriteLine("Backtracking finished. Found " + btSolutions.Count + " solutions");
+            //Diagnostics
+            generalStopwatch.Stop();
+            generalTime = generalStopwatch.ElapsedMilliseconds;
+            Console.WriteLine("Diagnostics for Hetmans Problem: \nTime until first solution: {0}\nNodes visited: {1}\nTime in general: {2}", time, nodesVisited, generalTime);
+            //Diagnostics End
         }
-        public void FibonacciBackTracking(Node node, int heuristicID)//0 => take next; 1 => take always biggest number in domain if possible
+        public void FibonacciBackTracking(Node node, int heuristicID, int solutionsLimit)//0 => take next; 1 => take always biggest number in domain if possible
         {
+            if (btSolutions.Count == solutionsLimit && solutionsLimit != -1)
+                return;
             Node next;
             bool isNewSolution;
             int satisfactionCheck;
@@ -98,12 +148,21 @@ namespace CSP
             {
                 for (int i = 0; i < node.domain.Count; i++)
                 {
+                    //Diagnostics
+                    nodesVisited++;
+                    //Diagnostics end
                     satisfactionCheck = node.SatisfactionCheck(node.domain[i]);
                     next = node.InsertToSolution(i);
                     if (satisfactionCheck == 0)
-                        FibonacciBackTracking(next, heuristicID);
+                        FibonacciBackTracking(next, heuristicID, solutionsLimit);
                     if (satisfactionCheck == 1)
                     {
+                        if (time == 0)//Diagnostics
+                        {
+                            stopwatch.Stop();
+                            time = stopwatch.ElapsedMilliseconds;
+                        }//Diagnostics End
+
                         isNewSolution = true;
                         foreach (var item in btSolutions)
                         {
@@ -113,7 +172,7 @@ namespace CSP
                         if (isNewSolution || btSolutions.Count == 0)
                         {
                             btSolutions.Add(next);
-                            PrintFibonacciScore(next, "bt");
+                            //PrintFibonacciScore(next, "bt");
                         }
                     }
                 }
@@ -122,12 +181,20 @@ namespace CSP
             {
                 for(int i = node.domain.Count - 1; i >= 0; i--)
                 {
+                    //Diagnostics
+                    nodesVisited++;
+                    //Diagnostics end
                     satisfactionCheck = node.SatisfactionCheck(node.domain[i]);
                     next = node.InsertToSolution(i);
                     if (satisfactionCheck == 0)
-                        FibonacciBackTracking(next, heuristicID);
+                        FibonacciBackTracking(next, heuristicID, solutionsLimit);
                     if (satisfactionCheck == 1)
                     {
+                        if (time == 0)//Diagnostics
+                        {
+                            stopwatch.Stop();
+                            time = stopwatch.ElapsedMilliseconds;
+                        }//Diagnostics End
                         isNewSolution = true;
                         foreach (var item in btSolutions)
                         {
@@ -137,7 +204,7 @@ namespace CSP
                         if (isNewSolution || btSolutions.Count == 0)
                         {
                             btSolutions.Add(next);
-                            PrintFibonacciScore(next, "bt");
+                            //PrintFibonacciScore(next, "bt");
                         }
                     }
                 }
@@ -148,28 +215,52 @@ namespace CSP
 
         #region HETMANS_PROBLEM
 
-        public void HetmansBackTracking()
+        public void HetmansBackTracking(int solutionsLimit)
         {
-            HetmansBacktracking(initialNode);
-            Console.WriteLine("Backtracking finished. Found "+btSolutions.Count+" solutions");
+            //Diagnostics
+            stopwatch = new Stopwatch();
+            time = 0;
+            nodesVisited = 0;
+            stopwatch.Start();
+            generalStopwatch = new Stopwatch();
+            generalTime = 0;
+            generalStopwatch.Start();
+            //Diagnostics end
+            HetmansBacktracking(initialNode, solutionsLimit);
+            Console.WriteLine("Backtracking finished. Found " + btSolutions.Count + " solutions");
+            //Diagnostics
+            generalStopwatch.Stop();
+            generalTime = generalStopwatch.ElapsedMilliseconds;
+            Console.WriteLine("Diagnostics for Hetmans Problem: \nTime until first solution: {0}\nNodes visited: {1}\nTime in general: {2}", time, nodesVisited, generalTime);
+            //Diagnostics End
         }
-        public void HetmansBacktracking(Node node)
+        public void HetmansBacktracking(Node node, int solutionsLimit)
         {
+            if (btSolutions.Count == solutionsLimit)
+                return;
             Node next;
             bool isNewSolution;
             for (int i = 0; i < problemSize; i++)
             {
                 for (int j = 0; j < problemSize; j++)
                 {
+                    //Diagnotics
+                    nodesVisited++;
+                    //Diagnostics end
                     if (node.SatisfactionCheck(i, j))
                     {
                         next = node.InsertHetman(i, j);
                         if (next.hetmansPlaced < problemSize)
                         {
-                            HetmansBacktracking(next);
+                            HetmansBacktracking(next, solutionsLimit);
                         }
                         else
                         {
+                            if (time == 0)//Diagnostics
+                            {
+                                stopwatch.Stop();
+                                time = stopwatch.ElapsedMilliseconds;
+                            }//Diagnostics End
                             isNewSolution = true;
                             foreach(var item in btSolutions)
                             {
@@ -179,20 +270,36 @@ namespace CSP
                             if (isNewSolution || btSolutions.Count == 0)
                             {
                                 btSolutions.Add(next);
-                                PrintHetmansScore(next, "bt");
+                                //PrintHetmansScore(next, "bt");
                             }
                         }
                     }
                 }
             }
         }
-        public void HetmansForwardChecking()
+        public void HetmansForwardChecking(int solutionsLimit)
         {
-            HetmansForwardChecking(initialNode);
+            //Diagnostics
+            stopwatch = new Stopwatch();
+            time = 0;
+            nodesVisited = 0;
+            stopwatch.Start();
+            generalStopwatch = new Stopwatch();
+            generalTime = 0;
+            generalStopwatch.Start();
+            //Diagnostics end
+            HetmansForwardChecking(initialNode, solutionsLimit);
             Console.WriteLine("Forward checking finished. Found " + fcSolutions.Count + " solutions");
+            //Diagnostics
+            generalStopwatch.Stop();
+            generalTime = generalStopwatch.ElapsedMilliseconds;
+            Console.WriteLine("Diagnostics for Hetmans Problem: \nTime until first solution: {0}\nNodes visited: {1}\nTime in general: {2}", time, nodesVisited, generalTime);
+            //Diagnostics End
         }
-        public void HetmansForwardChecking(Node node)
+        public void HetmansForwardChecking(Node node, int solutionsLimit)
         {
+            if (fcSolutions.Count == solutionsLimit)
+                return;
             List<Tuple<int, int>> possibleIndexes = new List<Tuple<int, int>>();
             bool isNewSolution;
             if (node.hetmansPlaced < problemSize)//if problem not satisfied
@@ -207,11 +314,19 @@ namespace CSP
                 }
                 foreach (var item in possibleIndexes)
                 {
-                    HetmansForwardChecking(node.InsertHetman(item.Item1, item.Item2));
+                    //Diagnotics
+                    nodesVisited++;
+                    //Diagnostics end
+                    HetmansForwardChecking(node.InsertHetman(item.Item1, item.Item2), solutionsLimit);
                 }
             }
             else
             {
+                if (time == 0)//Diagnostics
+                {
+                    stopwatch.Stop();
+                    time = stopwatch.ElapsedMilliseconds;
+                }//Diagnostics End
                 isNewSolution = true;
                 foreach (var item in fcSolutions)
                 {
@@ -221,7 +336,7 @@ namespace CSP
                 if (isNewSolution || fcSolutions.Count == 0)
                 {
                     fcSolutions.Add(node);
-                    PrintHetmansScore(node, "fc");
+                    //PrintHetmansScore(node, "fc");
                 }
             }
         }
--- CSP/Program.cs
+++ CSP/Program.cs
@@ -1,15 +1,53 @@
 ﻿
+using System;
+
 namespace CSP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Graph hetman = new Graph(7, 0); //0 means that its hetmans problem
-            //hetman.HetmansForwardChecking();
-            
-            Graph fibonacci = new Graph(59, 1);
-            fibonacci.FibonacciForwardChecking(1);
+            //Hetmans
+            //Graph hetmans;
+            //int[] hetmansTestData = new int[8] { 4, 5, 6, 7, 8, 9, 10, 11 };
+            //int[] hetmansSolution = new int[8] { 2, 10, 4, 40, 92, 352, 724, 2680 };
+            //for (int i = 5; i < 8; i++)
+            //{
+            //    Console.WriteLine("Hetmans problem for N={0}", hetmansTestData[i]);
+            //    hetmans = new Graph(hetmansTestData[i], 0);
+            //    Console.WriteLine();
+            //    if (i != 5)
+            //    {
+            //        Console.WriteLine("Forward checking");
+            //        hetmans.HetmansForwardChecking(hetmansSolution[i]);
+            //        Console.WriteLine();
+            //    }
+            //    Console.WriteLine("Backtracking");
+            //    hetmans.HetmansBackTracking(hetmansSolution[i]);
+            //    Console.WriteLine();
+            //}
+            //Fibonacci
+            Graph fibonacci;
+            int[] fibonacciTestData = new int[6] { 59, 617, 1447, 2137, 10177, 104009 };
+            //for problem size 617
+            Console.WriteLine("Fibonacci problem for N={0}", fibonacciTestData[1]);
+            fibonacci = new Graph(fibonacciTestData[1], 1);
+            Console.WriteLine();
+            Console.WriteLine("Forward checking with heuristic 0");
+            fibonacci.FibonacciForwardChecking(0, 5);
+            fibonacci = new Graph(fibonacciTestData[1], 1);
+            Console.WriteLine();
+            Console.WriteLine("Forward checking with heuristic 1");
+            fibonacci.FibonacciForwardChecking(1, 5);
+            fibonacci = new Graph(fibonacciTestData[1], 1);
+            Console.WriteLine();
+            Console.WriteLine("Backtracking with heuristic 0");
+            fibonacci.FibonacciBackTracking(0, 5);
+            fibonacci = new Graph(fibonacciTestData[1], 1);
+            Console.WriteLine();
+            Console.WriteLine("Backtracking with heuristic 1");
+            fibonacci.FibonacciBackTracking(1, 5);
+            Console.WriteLine();
         }
     }
 }
